556249a114bfa535fc44aac7b78f1eb5bc30b98f
tententgc <nyman3527@gmail.com>
2021-07-19 21:44:31 +0700

    motion tracking update

--- moiton detection.py
+++ moiton detection.py
@@ -1,6 +1,6 @@
 #Motion Detection
 import cv2
-cap = cv2.VideoCapture("image/Walking.mp4")
+cap = cv2.VideoCapture("video/blackpink.mp4")
 check, frame1 = cap.read()
 check, frame2 = cap.read()
 while (cap.isOpened()):
@@ -11,12 +11,17 @@ while (cap.isOpened()):
         thresh, result = cv2.threshold(blur, 15, 255, cv2.THRESH_BINARY)
         dilation = cv2.dilate(result, None, iterations=3)
         contours, hierarchy = cv2.findContours(
-            dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+        dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        for contour in contours:
+            (x,y,w,h) = cv2.boundingRect(contour)
+            if cv2.contourArea(contour)<2500:
+                continue
+            cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
         cv2.imshow("Output", frame1)
+        
         frame1 = frame2
         check, frame2 = cap.read()
-        if cv2.waitKey(1) & 0xFF == ord("e"):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         break
